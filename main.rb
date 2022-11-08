399a1d80ba8f9861b2ca96d1384725bcb5403b8a
ShahidHaris <alharithshaheed@yahoo.com>
2022-11-08 08:46:14 +0100

    add the suggestion to help make the best stock exchange

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
-def stock_picker(stock_prices)
+def stock_picker(prices)
     #making a copy of the stock_prices
-    cloned = stock_prices.dup
+    cloned = prices.dup
 
     #getting the days buying and selling
     def days_of_exchange(days)
@@ -26,4 +26,13 @@ def stock_picker(stock_prices)
         end
     end
 
+    days_combination = days_of_exchange(prices)[0].flatten(1)
+    returns = stock_returns(prices).flatten
+    max_return = returns.max 
+    best_days = days_combination[returns.find_index(max_return)]
+
+    suggestion = "#{best_days} # for profit of $#{cloned[best_days[1]]} - $#{cloned[best_days[0]]} == $#{max_return}"
+
 end
+
+array = stock_picker([17,3,6,9,15,8,6,1,10])
